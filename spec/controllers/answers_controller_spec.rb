4302dacc4c0b7b7fc5c1328138f80dede62c37d4
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2022-08-31 15:24:52 +0600

    test complete saves new answer to db (phew)

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,9 +1,15 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
   describe 'POST #create' do
+    let(:answer) { create(:answer) }
+
     context 'with valid attributes'
-    it 'saves new answer to database'
+    it 'saves new answer to database' do
+      expect { post :create, params: { answer: attributes_for(:answer), question_id: answer.question.id } }.to change(Answer, :count).by(1)
+    end
     it 'redirects to show view'
 
     context 'with invalid attributes'
