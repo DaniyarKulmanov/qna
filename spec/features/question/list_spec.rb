318e76ab95831b60477de85a9184c21c9e96f3d4
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2022-09-05 17:41:07 +0600

    User can see all questions - init

---
+++ spec/features/question/list_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+feature 'User can see all questions', "
+  In order get all questions from community
+  As an unauthorized or authorized user
+  I'd like to see all questions
+" do
+
+  let(:questions) { create_list(:question, 3) }
+
+  scenario 'Unregistered user tries to see all questions' do
+    visit questions_path
+
+    questions.each do |question|
+      expect(page).to have_content question.title
+      expect(page).to have_content question.body
+    end
+  end
+
+  scenario 'Registered user tries to see all questions' do
+    given(:user) { create(:user) }
+    sign_in(user)
+    visit questions_path
+
+    questions.each do |question|
+      expect(page).to have_content question.title
+      expect(page).to have_content question.body
+    end
+  end
+end
