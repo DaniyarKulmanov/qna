dbcf949d769bae1501df07c5d1dfdc429b9f533b
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2022-09-05 15:42:43 +0600

    create_spec.rb

--- spec/question/create_spec.rb
+++ spec/question/create_spec.rb
@@ -8,38 +8,33 @@ feature 'User can create question', "
   I'd like to be able ask the question
 " do
 
-  given(:user) { User.create!(email: 'user@test.com', password: '12345678') }
+  given(:user) { create(:user) }
+  given(:question) { create(:question) }
 
-  background { visit new_user_session_path }
+  describe 'Authenticated user' do
 
-  scenario 'Authorized user asks question' do
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    click_on 'Log in'
+    background do
+      sign_in(user)
 
-    visit questions_path
-    click_on 'Ask question'
+      visit questions_path
+      click_on 'Ask question'
+    end
 
-    fill_in 'Title', with: 'Test question'
-    fill_in 'Body', with: 'Test text'
-    click_on 'Ask'
+    scenario 'asks question' do
+      fill_in 'Title', with: question.title
+      fill_in 'Body', with: question.body
+      click_on 'Ask'
 
-    expect(page).to have_content 'Your question successfully created'
-    expect(page).to have_content 'Test question'
-    expect(page).to have_content 'Test text'
-  end
+      expect(page).to have_content 'Your question successfully created'
+      expect(page).to have_content question.title
+      expect(page).to have_content question.body
+    end
 
-  scenario 'Authorized user asks question with errors' do
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    click_on 'Log in'
+    scenario 'asks question with errors' do
+      click_on 'Ask'
 
-    visit questions_path
-    click_on 'Ask question'
-
-    click_on 'Ask'
-
-    expect(page).to have_content "Title can't be blank"
+      expect(page).to have_content "Title can't be blank"
+    end
   end
 
   scenario 'Unauthorized user tries to ask question' do
