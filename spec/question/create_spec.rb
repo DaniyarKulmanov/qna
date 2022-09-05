207f3076c79d9b20e1762a94f15a2dc09622e643
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2022-09-05 14:48:31 +0600

    Test complete scenario: Authorized user asks question

---
+++ spec/question/create_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+feature 'User can create question', "
+  In order to get answer from community
+  As an authenticated user
+  I'd like to be able ask the question
+" do
+
+  given(:user) { User.create!(email: 'user@test.com', password: '12345678') }
+
+  background { visit new_user_session_path }
+
+  scenario 'Authorized user asks question' do
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_on 'Log in'
+
+    visit questions_path
+    click_on 'Ask question'
+
+    fill_in 'Title', with: 'Test question'
+    fill_in 'Body', with: 'Test text'
+    click_on 'Ask'
+
+    expect(page).to have_content 'Your question successfully created'
+    expect(page).to have_content 'Test question'
+    expect(page).to have_content 'Test text'
+  end
+
+  scenario 'Authorized user asks question with errors'
+  scenario 'Unauthorized user tries to ask question'
+
+end
