16d83d305afcdbd2edc7828125bf7f1caafd6ff7
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2022-09-06 10:15:09 +0600

    User can give answer to a question init

---
+++ spec/features/question/answer_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+feature 'User can give answer to a question', "
+  In order to give answer to question
+  As an authenticated user
+  I'd like to be able answer to question
+" do
+
+end
