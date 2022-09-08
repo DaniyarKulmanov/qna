418d14d37698e7eb46dc247d09b54267a361167a
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2022-09-08 19:58:03 +0600

    Any user can view question and its answers green

--- spec/features/question/show_spec.rb
+++ spec/features/question/show_spec.rb
@@ -8,8 +8,32 @@ feature 'Any user can view question and its answers', "
   I'd like to be able view questions and its answers
 " do
 
-  fscenario 'Authenticated user can see question and its answers' do
-    
+  given(:user) { create(:user) }
+  given(:question) { create(:question) }
+  given!(:answers) { create_list(:answer, 3, question: question) }
+
+  scenario 'Authenticated user can see question and its answers' do
+    sign_in(user)
+    visit question_path(question)
+
+    expect(page).to have_content question.title
+    expect(page).to have_content question.body
+
+    question.answers do |answer|
+      expect(page).to have_content answer.body
+      expect(page).to have_content answer.correct
+    end
   end
-  scenario 'Unauthenticated user can see question and its answers'
+
+  scenario 'Unauthenticated user can see question and its answers' do
+    visit question_path(question)
+
+    expect(page).to have_content question.title
+    expect(page).to have_content question.body
+
+    question.answers do |answer|
+      expect(page).to have_content answer.body
+      expect(page).to have_content answer.correct
+    end
+  end
 end
