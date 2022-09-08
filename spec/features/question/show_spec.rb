c82edba77a7684513d29db5619e4f0e47c2b7131
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2022-09-08 19:45:42 +0600

    Any user can view question and its answers init

---
+++ spec/features/question/show_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+feature 'Any user can view question and its answers', "
+  In order to find answers for a question
+  As any user
+  I'd like to be able view questions and its answers
+" do
+
+  fscenario 'Authenticated user can see question and its answers' do
+    
+  end
+  scenario 'Unauthenticated user can see question and its answers'
+end
