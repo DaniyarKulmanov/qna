efc514a9be4a73064212232d1f9b7442466ef9dd
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2022-09-09 09:49:34 +0600

    User can sign up init

---
+++ spec/features/authentication/sign_up_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+feature 'User can sign up', "
+  In order to get full functions
+  As Unauthenticated user
+  I'd like to be able sign up
+" do
+
+  scenario 'Unregistered user tries to sign up'
+  scenario 'Registered user tries to sign up'
+
+end
