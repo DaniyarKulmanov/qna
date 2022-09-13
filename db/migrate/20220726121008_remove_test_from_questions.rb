56374b48c86c5692ed3ec4846c4fd73b033ee5e8
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2022-09-13 09:06:03 +0600

    added author to answers

--- db/migrate/20220726121008_remove_test_from_questions.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTestFromQuestions < ActiveRecord::Migration[6.1]
-  def change
-    remove_reference :questions, :test, null: false, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_05_035801) do
+ActiveRecord::Schema.define(version: 2022_09_13_025408) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,19 +21,14 @@ ActiveRecord::Schema.define(version: 2022_09_05_035801) do
     t.bigint "question_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_answers_on_author_id"
     t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
   create_table "questions", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "tests", force: :cascade do |t|
-    t.string "title", null: false
-    t.integer "level", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -51,4 +46,5 @@ ActiveRecord::Schema.define(version: 2022_09_05_035801) do
   end
 
   add_foreign_key "answers", "questions"
+  add_foreign_key "answers", "users", column: "author_id"
 end
