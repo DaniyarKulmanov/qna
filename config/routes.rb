ad7c37d59cfe1ade01311d0a40923a3ecf72ff26
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2022-09-30 12:32:35 +0600

    #183323389 Unauthenticated user can not edit answer red

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   root to: 'questions#index'
 
   resources :questions do
-    resources :answers
+    resources :answers, shallow: true, only: %i[create update]
   end
 end
--- spec/features/answer/edit_spec.rb
+++ spec/features/answer/edit_spec.rb
@@ -8,17 +8,32 @@ feature 'User can edit his answer', "
   I'd like to be able to edit my answer
 " do
 
+  given!(:user) { create(:user) }
   given!(:question) { create(:question) }
-  given!(:answer) { create(:answer, question: question) }
+  given!(:answer) { create(:answer, question: question, author: user) }
 
   scenario 'Unauthenticated user can not edit answer' do
-    visit question_path(question)
+    visit questions_path(question)
 
     expect(page).to_not have_link 'Edit'
   end
 
   describe 'Authenticated user' do
-    scenario 'edits his answer'
+    scenario 'edits his answer' do
+      sign_in(user)
+      visit question_path(question)
+
+      click_on 'edit'
+
+      within '.answers' do
+        fill_in 'Body', with: 'edited answer'
+        click_on 'save'
+
+        expect(page).to_not have_content answer.body
+        expect(page).to have_content 'edited answer'
+        expect(page).to_not have_selector 'textarea'
+      end
+    end
 
     scenario 'edits his answer with errors'
     scenario "tries to edit other user's question"
