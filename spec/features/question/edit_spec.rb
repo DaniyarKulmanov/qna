94e01819ee172971fd74b15be5677756b49afd65
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2023-02-14 09:17:26 +0600

    User can edit his question:initial

---
+++ spec/features/question/edit_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+feature 'User can edit his question', "
+  In order to correct mistakes
+  As an author of question
+  I'd like to be able to edit my question
+" do
+
+end
