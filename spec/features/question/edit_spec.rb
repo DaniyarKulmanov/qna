6d65dadbea92f8d798df1a7e6144bc523391a465
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2023-02-14 11:05:39 +0600

    Unauthenticated user can not edit or delete answer:refactor-green

--- spec/features/question/edit_spec.rb
+++ spec/features/question/edit_spec.rb
@@ -8,4 +8,23 @@ feature 'User can edit his question', "
   I'd like to be able to edit my question
 " do
 
+  given!(:user) { create(:user) }
+
+  scenario 'Unauthenticated user can not edit or delete question' do
+    visit questions_path
+
+    expect(page).to_not have_link 'edit'
+    expect(page).to_not have_link 'delete'
+  end
+
+  describe 'Authenticated user', js: true do
+    describe 'edits his' do
+      scenario 'question'
+      scenario 'question with errors'
+    end
+
+    describe "tries edit other user's" do
+      scenario 'question with errors'
+    end
+  end
 end
